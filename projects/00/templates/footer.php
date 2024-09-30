81b5b0028a358b62ecd1777ce91aff9e9ec0bb4f
shannontuckett <lesleytuckett@mail.weber.edu>
2024-09-30 16:34:19 +0000

    Created project 00 folder

---
+++ projects/00/templates/footer.php
@@ -0,0 +1,16 @@
+</main>
+    <!-- END MAIN PAGE CONTENT -->
+
+    <!-- BEGIN PAGE FOOTER -->
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>My Webpage</strong> by <a href="#">Your Name</a>. The source code is licensed under MIT.
+            </p>
+        </div>
+    </footer>
+    <!-- END PAGE FOOTER -->
+
+</body>
+    
+</html>
---
+++ projects/00/templates/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Webpage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0/css/bulma.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@vizuaalog/bulmajs@0.12/dist/bulma.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+</head>
+
+<body class="has-navbar-fixed-top">
