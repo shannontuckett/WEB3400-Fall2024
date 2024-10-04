0985e2c0372c2958ad118f0bdc5dc6d128c27126
shannontuckett <lesleytuckett@mail.weber.edu>
2024-10-04 17:32:01 +0000

    Update project 01

---
+++ projects/01/login.php
@@ -0,0 +1,35 @@
+<?php
+include 'config.php';
+
+//We will code this together in class
+
+?>
+
+<!-- BEGIN YOUR CONTENT -->
+<section class="section">
+    <h1 class="title">Login</h1>
+    <form class="box" action="login.php" method="post">
+        <!-- Email -->
+        <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+                <input class="input" type="email" name="email" required>
+            </div>
+        </div>
+        <!-- Password -->
+        <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+                <input class="input" type="password" name="password" required>
+            </div>
+        </div>
+        <!-- Submit Button -->
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Login</button>
+            </div>
+        </div>
+    </form>
+    <a href="register.php" class="is-link"><strong>Create a new user account</strong></a>
+</section>
+<!-- END YOUR CONTENT -->
